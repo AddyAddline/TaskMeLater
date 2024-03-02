fde5c93d1169d5755e7af4d01e2f90bf6d70eada
AddyAddline <21ucc013@lnmiit.ac.in>
2024-03-02 20:44:41 +0530

    Add dependencies and routes, implement cron jobs and error handling middleware

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express=require('express');
 const app=express();
+const {notFound, errorHandler} = require('./middlewares/errorhandler.middleware');
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 
 app.use("/api/v1/user",require('./routes/user.routes'));
+app.use("/api/v1/tasks",require('./routes/task.routes'));
+app.use("/api/v1/subtasks",require('./routes/subtask.routes'));
+
+// Load cron jobs
+require("./cron-jobs/taskPriorityUpdater");
+require("./cron-jobs/twilioVoiceCaller");
+
+// Error handling middleware
+app.use(notFound);
+app.use(errorHandler);
 module.exports = app;
